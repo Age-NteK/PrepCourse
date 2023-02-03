cabc297a7a6e85c551db252f9de8834bad6f4cfd
Age-NteK <107895191+Age-NteK@users.noreply.github.com>
2023-02-03 16:57:34 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,19 @@
+function sumaTres(x){
+    console.log(x + 3)
+}
+sumaTres(10);
+
+//primero
+function sumaPrimero(x) {
+    console.log (x + 3);
+}
+sumaPrimero(15);
+//segundo
+var sumaSegundo = function (x) {
+    console.log (x + 3);
+}
+sumaSegundo(25);
+//tercero
+var sumaTercero = (x) => {console.log (x + 3);}
+sumaTercero(36);
+
