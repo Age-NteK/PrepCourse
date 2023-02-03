b7ff3a1022e3a7237570cf7f05070ee6a0552c34
Age-NteK <107895191+Age-NteK@users.noreply.github.com>
2023-02-03 16:43:57 -0300

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,48 @@
+
+ //suma
+console.log(5 + 7);
+
+//resta
+console.log(8 - 10);
+
+//multiplicación
+console.log(10 * 20);
+
+//división
+console.log(10 / 2);
+
+//módulo
+console.log(11 % 35);
+
+//precedencia
+console.log((8 + 5) * 6);
+
+//string
+console.log("Hola");
+
+//concatenación
+var nombre = "Macarena";
+var saludo = "Hola me llamo ";
+console.log(saludo + nombre);
+
+//mayor
+console.log(10 > 50);
+
+//menor
+console.log(10 < 50);
+
+//mayor o igual
+console.log( 10 >= 10);
+
+//valor
+console.log(10 == "10");
+
+//valor y tipo de dato
+console.log(10 === "10");
+
+//asignacion y comparación
+var a = 10;
+var b = 20;
+var c = a = b;
+console.log (c);
+
